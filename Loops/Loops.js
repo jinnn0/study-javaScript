fa5e20f74668a87295f2f316725e2c08f1dfa39d
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-02-12 00:29:51 +0000

    Loops

---
+++ Loops/Loops.js
@@ -0,0 +1,46 @@
+/**
+ * Loops
+
+ * For loop
+ * For in loop
+ * For of loop
+ */
+
+/*
+ * For loop
+ * Loops through a block of code a number of times
+ */
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+/*
+ * For in loop
+ * Loops through the properties / key of an object
+ */
+const song = {
+  title: 'untitled',
+  artist: 'Daniel',
+  genre: 'pop'
+};
+
+for (key in song) {
+  console.log(key, song[key]);
+}
+
+/*
+ * For of loop
+ * Loops through the values of an iterable object including Array, Map, Set and more
+ */
+const array = [3, 5, 7];
+array.foo = 'hello';
+
+const set = new Set('string');
+
+for (let item in array) {
+  console.log(item); // logs "0", "1", "2", "foo"
+}
+
+for (let item of set) {
+  console.log(item); // logs "s", "t", "r", "i", "n", "g"
+}
