a1c2aa2d6089a91a40bbad27a8cc6d3b1635cb35
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-02-19 11:56:28 +0000

    Add ES6 let & const

---
+++ ES6 - let & const/ES6 - let & const.js
@@ -0,0 +1,19 @@
+/*
+* var
+* With "var", the variable declaration could be overridden.
+* This becomes problem since this behavior doesn't throw an error
+  and allow you to overwrite a variable that you didn't intend
+  -> makes fixing bugs more difficu
+
+* let & const
+* Variable declaration
+  * Variable with the same name can only be declared once.
+* Scopes
+  * Block scope
+  
+* const
+* Has extra feature on top of let - constant value
+* Once a variable assigned with const can not be reassigned
+* Rrevents accidentally reassigning a variable
+* Note : object, array, functions declared with const are still mutable
+*/
