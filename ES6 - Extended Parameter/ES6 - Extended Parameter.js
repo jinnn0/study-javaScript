cb43736c3614b720acdb325c6bcd62fef9e79723
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-02-22 12:59:23 +0000

    Update ES6 Extended Parameter

--- ES6 - Extended Parameter/ES6 - Extended Parameter.js
+++ ES6 - Extended Parameter/ES6 - Extended Parameter.js
@@ -2,69 +2,75 @@
  * Extended Parameter handling
  */
 
-/** 
+/**
  * Default parameters - Before ES6
  */
-function showMessage(x, message, from){
-   if(x === undefined){
-      x = 'new'
-   }
+function showMessage(x, message, from) {
+  if (x === undefined) {
+    x = 'new';
+  }
 
-   if(from === undefined){
-     from = "unknown"
-   }
+  if (from === undefined) {
+    from = 'unknown';
+  }
 
-   console.log(`${message} by ${from}`)
+  console.log(`${message} by ${from}`);
 }
 
-showMessage("Hello!")
-
+showMessage('Hello!');
 
 /**
-* Default parameters - ES6
-*/
-function showMessage(x = 'new', message, from = 'unknown'){
-   console.log(`${x} ${message} by ${from}, `)
+ * Default parameters - ES6
+ */
+function showMessage(x = 'new', message, from = 'unknown') {
+  console.log(`${x} ${message} by ${from}, `);
 }
 
-showMessage("Hello!")
-
+showMessage('Hello!');
 
 /**
  * Rest parameters - Before ES6
  */
-function printList (param1, param2) {
-    const args = Array.prototype.slice.call(arguments, 2);
-    args.forEach(arg => console.log(arg))
+function printList(param1, param2) {
+  const args = Array.prototype.slice.call(arguments, 2);
+  args.forEach((arg) => console.log(arg));
+}
+
+printList('param1', 'param2', 'one', 'two', 'three');
+
+const sum = (x, y, z) => {
+  const args = [x, y, z];
+  return args.reduce((a, b) => a + b, 0);
 };
-
-printList('param1', 'param2', 'one', 'two', 'three')
-
 
 /**
  * Rest parameters - ES6
  */
-function printList(param1, param2, ...args){
-   for(let i = 0; i < args.length; i++ ){
-       console.log(args[i])
-   }
+function printList(param1, param2, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
 
-   for(const arg of args){
-       console.log(arg)
-   }
+  for (const arg of args) {
+    console.log(arg);
+  }
 
-   args.forEach(arg => console.log(arg))
+  args.forEach((arg) => console.log(arg));
 }
 
-printList('param1', 'param2', 'one', 'two', 'three') // args.length : 3, output : 'one', 'two', 'three'
+printList('param1', 'param2', 'one', 'two', 'three'); // args.length : 3, output : 'one', 'two', 'three'
 //  printList('param1', 'param2', ['one', 'two', 'three']) // ...args.length : 1, output : ['one', 'two', 'three']
 
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
+
 /**
- * Rest parameters / Spread Operator - ES6 
+ * Rest parameters / Spread Operator - ES6
  */
-function printList(param1, param2, ...args){ 
-   args.forEach(arg => console.log(arg))
+function printList(param1, param2, ...args) {
+  args.forEach((arg) => console.log(arg));
 }
 
-const args = ['one', 'two', 'three']
-printList('param1', 'param2', ...args) 
+const args = ['one', 'two', 'three'];
+printList('param1', 'param2', ...args);
