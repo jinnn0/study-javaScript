c3d35cbfa68bc9ef5155f4d27e666067d2778933
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-02-25 21:18:18 +0000

    Add Scope & Context

---
+++ Scope & Context/Scope & Context.js
@@ -0,0 +1,15 @@
+/*
+ * Scope
+ * has to do with "variables"
+ * scope is a "one way" street
+ * inside code can reach outward for variables
+ * but, outside code can't reach inward for variables
+
+ * Context
+ * has to do with objects
+ * It refers to the object where the function belongs to
+ * "this" keyword
+ * In a method of an object -> "this" points towards the owner object
+ * In a regular function -> "this" points towards the global "window" object (in strict mode "this" is undefined)
+ * In an arrow function -> "this"
+ */
