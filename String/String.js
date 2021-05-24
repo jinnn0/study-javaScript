d3aca107aaaa4ba2a6f9838000c5d0b419c7fa69
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2021-05-24 11:38:09 +0100

    Update String.js - add simplified list

--- String/String.js
+++ String/String.js
@@ -89,15 +89,51 @@ console.log(two.trim());
 console.log(three.replace('an', 'oo')); // only the first occurrence => There is a booana and an orange,
 console.log(three.replace(/an/g, 'oo')); // all occurrences => There is a booooa ood oo orooge,
 
-// 18. repeat() - returns a new string
+// 18. repeat() - returns a new string which contains the specified number of copies
 console.log(four.repeat(3)); // HELLOHELLOHELLO
 
-// 19. toUpperCase() - returns a new string
+// 19. toUpperCase() - returns a new string that is converted to lowercase letters
 console.log(three.toUpperCase()); // THERE IS A BANANA AND AN ORANGE,
 
-// 20. toLowerCase() - returns a new string
+// 20. toLowerCase() - returns a new string that is converted to uppercase letters
 console.log(four.toLocaleLowerCase()); // hello
 
 // 21. toString()
 const num = 12;
 console.log(num.toString()); // "12"
+
+/**
+ * Simplified list!
+ */
+// 01. concat() : returns a two or more joined string
+
+// 02. charAt() : returns a UTF-16 code of specified character
+// 03. charCodeAt() : returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index
+// 04. fromCharCode() :  returns a string created from the specified sequence of UTF-16 code units
+
+// 05. indexOf() : returns an index of the first occurrence of the specified value, returns -1 if doesn't exist
+// 06. lastIndexOf() : returns an index of the last occurrence of the specified value, search backwards, returns -1 if doesn't exist
+// 07. search() : returns the index of the first match from regular expression, -1 if no match is found
+
+// 08. match() : returns an array of matches found, null if no matches are found
+
+// 09. includes() : returns true/false depends on whether the string in included or not, case-sensitive search
+// 10. endsWith() : true/false, whether a string ends with the characters of a specified string
+// 11. startsWith() : true/false, whether a string starts with the characters of a specified string
+
+// 12. slice() : returns a new string of extracted section of the string
+// 13. substr() : returns a portion of the string, from startPos and to endPost but not including
+// 14. substring()
+
+// 15. split() : returns an array of split string
+// 16. trim() : returns a new string with whitespace from both ends of a string removed
+// 17. replace() : returns s new string with value replaced
+// 18. repeat() : returns a new string which contains the specified number of copies
+// 19. toUpperCase() : returns a new string that is converted to lowercase letters
+// 20. toLowerCase() : returns a new string that is converted to uppercase letters
+// 21. toString() : returns a string representing the specified object
+
+// 22. localeCompare()
+// 23. tolocaleLowerCase()
+// 24. toLocaleUpperCase()
+// 25. valueOf()
