57315b73cd59dc18ad7912763329c4d51ac742ee
jinnn0 <Jinyoung.jeong20@gmail.com>
2021-06-13 14:48:26 +0100

    Update Loops.js

--- Loops/Loops.js
+++ Loops/Loops.js
@@ -1,22 +1,24 @@
 /**
  * Loops
+ 
+ * Loops can execute a block of code a number of time
 
- * For loop
- * For in loop
- * For of loop
+ * For      : loops through a block of code a number of times until a specified condition evaluates to "false"
+ * For in   : loops through the properties / keys of an object
+ * For of   : loops through the values of an iterable object (Array, Map, Set, String, etc)
+ * while    : loops through a block of code while a specified condition is true
+ * do while : same as while loop
  */
 
 /*
  * For loop
- * Loops through a block of code a number of times
  */
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
 /*
- * For in loop
- * Loops through the properties / key of an object
+ * For in 
  */
 const song = {
   title: 'untitled',
@@ -24,18 +26,27 @@ const song = {
   genre: 'pop'
 };
 
+// Variable : a different property name is assigned to variable on each iteration.
+for (variable in object) {
+  ...statement
+}
+
 for (key in song) {
   console.log(key, song[key]); // title, artist, genre
 }
 
 /*
- * For of loop (added in ES6)
- * Loops through the values of an iterable object including Array, Map, Set and more
+ * For of  (added in ES6)
  */
 const array = [3, 5, 7];
 array.foo = 'hello';
 
 const set = new Set('string');
+
+// variable : on each literation a value of a different property is assigned to variable
+for (variable of iterable) {
+  ...statement
+}
 
 for (let item in array) {
   console.log(item); // logs "0", "1", "2", "foo"
@@ -44,3 +55,27 @@ for (let item in array) {
 for (let item of set) {
   console.log(item); // logs "s", "t", "r", "i", "n", "g"
 }
+
+
+/*
+ * While
+ */
+let i = 0;
+while (i < 5) {
+  i++;
+  console.log(i); // 1,2,3,4,5
+}
+
+
+/*
+ * Do while
+ */
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result += i;
+} while (i < 5); // "12345"
+
+
